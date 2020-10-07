8eb635c43c1e431f3ab0da87036471cd8c476134
Suizhazha <573505403@qq.com>
2020-10-07 12:56:20 +0800

    update

---
+++ precache-manifest.9c61f4703baa469cc576c69fac382250.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49f6e256c1e0744bd79ea5ae59b7cf25",
+    "url": "/money-website-react/index.html"
+  },
+  {
+    "revision": "a5a8c6f8a35b0db49737",
+    "url": "/money-website-react/static/css/main.09a97450.chunk.css"
+  },
+  {
+    "revision": "01fe39667e4dae8bca08",
+    "url": "/money-website-react/static/js/2.a10286f1.chunk.js"
+  },
+  {
+    "revision": "768165ae6e4455b0f754a8444c7b06fa",
+    "url": "/money-website-react/static/js/2.a10286f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5a8c6f8a35b0db49737",
+    "url": "/money-website-react/static/js/main.67720975.chunk.js"
+  },
+  {
+    "revision": "07760577602358430abb",
+    "url": "/money-website-react/static/js/runtime-main.42b7001a.js"
+  }
+]);
